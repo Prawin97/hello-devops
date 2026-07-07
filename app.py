0d128db322565bfe05052ed79e281927a1e92b94
Prawin97 <Prawinkothaplly07@gmail.com>
2026-07-07 14:13:10 +0530

    updated version

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>🚀 Hello DevOps v3</h1>
-    <h2>Version 2</h2>
-    <p>Welcome to my CI/CD Pipeline</p>
+    <h1>🚀 Hello Praveen</h1>
+    <h2>Welcome to DBS</h2>
+    <p>CI/CD Pipeline</p>
     """
 
 if __name__ == "__main__":
